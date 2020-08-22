30c91fd6c266628325ad77d886a6c85a38809fe8
Sarwar-Ahmed <sarwarahmed109@gmail.com>
2020-08-22 22:13:37 +0600

    stack in totalOpenIssue

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ function submitIssue(e) {
 
   const issue = { id, description, severity, assignedTo, status };
   let issues = [];
+  
   if (localStorage.getItem('issues')){
     issues = JSON.parse(localStorage.getItem('issues'));
   }
@@ -17,12 +18,34 @@ function submitIssue(e) {
   localStorage.setItem('issues', JSON.stringify(issues));
 
   document.getElementById('issueInputForm').reset();
+  totalIssue();
+  totalOpenIssue();
   fetchIssues();
   e.preventDefault();
 }
 
+const totalIssue = () => {
+  const issues = JSON.parse(localStorage.getItem("issues")) || [];
+  document.getElementById("totalIssue").innerText = issues.length;
+
+}
+// totalIssue();
+const totalOpenIssue = () => {
+  const issues = JSON.parse(localStorage.getItem("issues")) || [];
+  const count = 0;
+  issues.forEach(element => {
+    if(element.status === "Open"){
+      count+=1;
+    }
+  });
+  console.log(count)
+  document.getElementById("totalOpenIssue").innerText = count;
+}
+
+// totalOpenIssue();
 const closeIssue = id => {
   const issues = JSON.parse(localStorage.getItem('issues'));
+  
   const currentIssue = issues.find(issue => issue.id === id);
   currentIssue.status = 'Closed';
   localStorage.setItem('issues', JSON.stringify(issues));
@@ -31,7 +54,8 @@ const closeIssue = id => {
 
 const deleteIssue = id => {
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const remainingIssues = issues.filter( issue.id !== id )
+  const remainingIssues = issues.filter(issue => issue.id != id );
+  console.log(remainingIssues)
   localStorage.setItem('issues', JSON.stringify(remainingIssues));
 }
 
@@ -39,17 +63,17 @@ const fetchIssues = () => {
   const issues = JSON.parse(localStorage.getItem('issues'));
   const issuesList = document.getElementById('issuesList');
   issuesList.innerHTML = '';
-
+  document.getElementById("totalIssue").innerText = issues.length;
   for (var i = 0; i < issues.length; i++) {
     const {id, description, severity, assignedTo, status} = issues[i];
 
-    issuesList.innerHTML +=   `<div class="well">
+    issuesList.innerHTML +=   `<div id="issue-card-${id}"  class="well">
                               <h6>Issue ID: ${id} </h6>
                               <p><span class="label label-info"> ${status} </span></p>
                               <h3> ${description} </h3>
                               <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
                               <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-                              <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
+                              <a href="#" onclick="closeIssue(${id})" class="btn btn-warning">Close</a>
                               <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
                               </div>`;
   }
