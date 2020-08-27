683730a55256f6c6bc7c7687ca5985dbc92dfcf7
Sarwar-Ahmed <sarwarahmed109@gmail.com>
2020-08-27 20:09:33 +0600

    confused

--- main.js
+++ main.js
@@ -29,52 +29,57 @@ const totalIssue = () => {
   document.getElementById("totalIssue").innerText = issues.length;
 
 }
-// totalIssue();
+totalIssue();
 const totalOpenIssue = () => {
   const issues = JSON.parse(localStorage.getItem("issues")) || [];
   const count = 0;
   issues.forEach(element => {
     if(element.status === "Open"){
-      count+=1;
+      count +=1;
     }
   });
-  console.log(count)
+  // console.log(count)
   document.getElementById("totalOpenIssue").innerText = count;
 }
 
 // totalOpenIssue();
-const closeIssue = id => {
+const closeIssue = (event, id) => {
+  event.preventDefault();
   const issues = JSON.parse(localStorage.getItem('issues'));
   
-  const currentIssue = issues.find(issue => issue.id === id);
+  const currentIssue = issues.find(issue => issue.id == id);
   currentIssue.status = 'Closed';
   localStorage.setItem('issues', JSON.stringify(issues));
   fetchIssues();
+  totalOpenIssue();
+  document.getElementById(`issue-title-${id}`).style.textDecoration = "line-through";
 }
 
-const deleteIssue = id => {
+const deleteIssue = (event, id) => {
+  event.preventDefault();
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const remainingIssues = issues.filter(issue => issue.id != id );
-  console.log(remainingIssues)
+  const remainingIssues = issues.filter(issue => issue.id != id )
+  document.getElementById(`issue-card-${id}`).style.display = "none";
   localStorage.setItem('issues', JSON.stringify(remainingIssues));
+  totalIssue();
+  totalOpenIssue();
 }
 
 const fetchIssues = () => {
   const issues = JSON.parse(localStorage.getItem('issues'));
   const issuesList = document.getElementById('issuesList');
   issuesList.innerHTML = '';
-  document.getElementById("totalIssue").innerText = issues.length;
   for (var i = 0; i < issues.length; i++) {
     const {id, description, severity, assignedTo, status} = issues[i];
 
     issuesList.innerHTML +=   `<div id="issue-card-${id}"  class="well">
                               <h6>Issue ID: ${id} </h6>
                               <p><span class="label label-info"> ${status} </span></p>
-                              <h3> ${description} </h3>
+                              <h3 id="issue-title-${id}"> ${description} </h3>
                               <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
                               <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-                              <a href="#" onclick="closeIssue(${id})" class="btn btn-warning">Close</a>
-                              <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
+                              <a href="#" onclick="closeIssue(event, ${id})" class="btn btn-warning">Close</a>
+                              <a href="#" onclick="deleteIssue(event, ${id})" class="btn btn-danger">Delete</a>
                               </div>`;
   }
 }
